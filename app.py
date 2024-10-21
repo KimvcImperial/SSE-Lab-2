1f33538a0166ef61c827be77359fd504da8604c8
Kimberley Cheung <kvc24@oak09.doc.ic.ac.uk>
2024-10-21 14:43:48 +0100

    pipelinetasks

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
-
 from flask import Flask, render_template, request
 
+# Two blank lines after imports
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
-    
     return render_template("index.html")
+
 
 @app.route("/submit", methods=["POST"])
 def submit():
     input_name = request.form.get("name")
     input_age = request.form.get("age")
     return render_template("hello.html", name=input_name, age=input_age)
+
+
+# Two blank lines before following code (if any)
