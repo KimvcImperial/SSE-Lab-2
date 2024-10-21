df836fbe8463796fe02b9fd6c7d5193f938d5c85
Kimberley Cheung <kvc24@oak09.doc.ic.ac.uk>
2024-10-21 17:20:08 +0100

    workdone

--- app.py
+++ app.py
@@ -2,6 +2,15 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+def process_query(query):
+    if query == "dinosaurs":
+        ans = "Dinosaurs ruled the Earth 200 million years ago"
+    elif query == "asteroids":
+        ans = "Unknown"
+    else:
+        ans = "No query provided"
+    return f"{ans}"
+
 
 @app.route("/")
 def hello_world():
@@ -13,3 +22,10 @@ def submit():
     input_name = request.form.get("name")
     input_age = request.form.get("age")
     return render_template("hello.html", name=input_name, age=input_age)
+
+@app.route("/query", methods=["GET"])
+def query():
+    query = request.args.get("q")
+    return process_query(query)
+
+
