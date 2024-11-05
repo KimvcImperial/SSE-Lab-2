633ce35c16e85ac298934acf93d17f79209d8271
Kimberley Cheung <kvc24@oak12.doc.ic.ac.uk>
2024-11-05 11:09:29 +0000

    completeapppy3

--- app.py
+++ app.py
@@ -141,15 +141,13 @@ def greet():
                         "stars": repo_data_stats.get("stargazers_count", 0),
                         "forks": repo_data_stats.get("forks_count", 0),
                         "watchers": repo_data_stats.get("watchers_count", 0),
-                        "language": repo_data_stats.get(
-                            "language", "N/A"),
+                        "language": repo_data_stats.get("language", "N/A"),
                         "open_issues": repo_data_stats.get("open_issues_count", 0),
                     }
                 )
 
         # Sorting repositories by creation date to get the most recent
-        sorted_repos = sorted(
-            repos, key=lambda x: x["created_at"], reverse=True)
+        sorted_repos = sorted(repos, key=lambda x: x["created_at"], reverse=True)
         for repo in sorted_repos[:5]:  # Get top 5 most recently created repos
             recent_repos.append(
                 {
@@ -164,8 +162,9 @@ def greet():
             username=username,
             repo_data=repo_data,
             repo_stats=repo_stats,  # Pass repository stats to the template
-            recent_repos=recent_repos,  
-            # Pass recent repositories to the template
+            recent_repos=recent_repos,
+            # Pass recent repositories 
+            #to the template
         )
     else:
         return render_template(
